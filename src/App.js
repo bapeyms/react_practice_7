28eb633b3a5a49d08781d9b9c8341f02aa7e7c99
serdka99 <serdka99@29-8>
2026-05-20 16:02:51 +0300

    process

--- src/App.js
+++ src/App.js
@@ -31,13 +31,33 @@ const testArr = [
       "Option 4"
     ],
     correct: 2
+  },
+  {
+    question: "Question 4?",
+    options: [
+      "Option 1",
+      "Option 2",
+      "Option 3",
+      "Option 4"
+    ],
+    correct: 4
+  },
+  {
+    question: "Question 5?",
+    options: [
+      "Option 1",
+      "Option 2",
+      "Option 3",
+      "Option 4"
+    ],
+    correct: 1
   }
 ]
 
 function App() {
   return (
     <div className="App">
-      
+      <Test obj={testArr[0]}></Test>
     </div>
   );
 }
--- src/components/Test/Test.js
+++ src/components/Test/Test.js
@@ -2,7 +2,26 @@ import './Test.css';
 
 export function Test(props) {
   return <>
-  <div>
+  <div className="Test">
+    <p>{props.obj.question}</p>
+    <ol type="A">
+      {props.obj.options.map((option, index) => (
+        <li key={index}>{option}</li>
+      ))}
+    </ol>
+    <p>Select an answer:</p>
+    <div className="answer-buttons">
+      <button>A</button>
+      <button>B</button>
+      <button>C</button>
+      <button>D</button>
+    </div>
+
+    <div className="choice-buttons">
+      <button>Skip</button>
+      <button>Select</button>
+      <button>Finish</button>
+    </div>
     
   </div>
   </>
